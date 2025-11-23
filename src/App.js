2603904301fe48f53cedcf6a9474ad579cebbfc0
Vandeth Meng <vandeth.ifl@gmail.com>
2025-11-23 06:06:37 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2> You have successfully testing Jenkins </h2>
         <a
           className="App-link"
           href="https://example.com"
